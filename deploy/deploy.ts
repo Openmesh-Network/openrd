04a17e30318ea355967e4e179a0b21201107c820
Plopmenz <plopmenz@gmail.com>
2024-01-31 23:27:53 +0100

    deployment parameters

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -1,6 +1,8 @@
 import { Address, Deployer } from "../web3webdeploy/types";
 
-export interface TasksDeploymentSettings {}
+export interface TasksDeploymentSettings {
+  disabler?: Address;
+}
 
 export interface TasksDeployment {
   tasks: Address;
@@ -10,10 +12,12 @@ export async function deploy(
   deployer: Deployer,
   settings?: TasksDeploymentSettings
 ): Promise<TasksDeployment> {
+  const disabler =
+    settings?.disabler ?? "0x2309762aAcA0a8F689463a42c0A6A84BE3A7ea51";
   const tasks = await deployer.deploy({
     id: "Tasks",
     contract: "Tasks",
-    args: ["0x2309762aAcA0a8F689463a42c0A6A84BE3A7ea51"],
+    args: [disabler],
   });
   return {
     tasks: tasks,
